8b41386e3a988028fc2c165c1308d57371ec08f1
celia77940 <celiarameau@live.fr>
2021-03-01 13:58:52 +0100

    crud participant fini

--- index.php
+++ index.php
@@ -2,7 +2,29 @@
 require_once ("inc/header.php");
 ?>
 
-<!-- contenu -->
+<form method="POST" action="crudParticipant.php" enctype="multipart/form-data">
+    <div>    
+        <label for="Nom">Nom</label> <br>
+        <input type="text" name="nom_participant">
+    </div>
+    <div>    
+        <label for="Prenom">Prenom</label> <br>
+        <input type="text" name="prenom_participant">
+    </div>
+    <div>    
+        <label for="Date">Date de naissance</label> <br>
+        <input type="date" name="birth_participant">
+    </div>
+    <div>    
+        <label for="mail">Mail</label> <br>
+        <input type="email" name="mail_participant">
+    </div>
+    <div>    
+        <label for="photo">Photo</label> <br>
+        <input type="file" name="img_participant">
+    </div> <br>
+    <button type="submit">Ajouter</button>
+</form>
 
 <?php
 require_once ("inc/footer.php");
