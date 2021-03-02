8ec57792f3abac43ef8364c1622681caa064270d
jctest <jcmairot@gmail.com>
2021-03-02 13:55:28 +0100

    readxlsx

---
+++ test_jc.php
@@ -0,0 +1,31 @@
+<?php
+include 'inc/header.php';
+include 'inc/fonctions.php';
+require 'vendor/autoload.php';
+
+$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Xlsx');
+$reader->setReadDataOnly(TRUE);
+$spreadsheet = $reader->load("creatXl.xlsx");
+
+$worksheet = $spreadsheet->getActiveSheet();
+// Get the highest row and column numbers referenced in the worksheet
+$highestRow = $worksheet->getHighestRow(); // e.g. 10
+$highestColumn = $worksheet->getHighestColumn(); // e.g 'F'
+$highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
+
+echo '<table>' . "\n";
+for ($row = 1; $row <= $highestRow; ++$row) {
+    echo '<tr>' . PHP_EOL;
+    for ($col = 1; $col <= $highestColumnIndex; ++$col) {
+        $value = $worksheet->getCellByColumnAndRow($col, $row)->getValue();
+        echo '<td>' . $value . '</td>' . PHP_EOL;
+    }
+    echo '</tr>' . PHP_EOL;
+}
+echo '</table>' . PHP_EOL;
+?> 
+
+
+<?php
+require_once 'inc/footer.php';
+?>
