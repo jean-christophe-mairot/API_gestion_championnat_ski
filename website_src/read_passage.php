3f07596805ad5bb47aa82db4e088c5cfcc6c6591
YoannGenique <genique.yoann@outlook.com>
2021-03-03 11:06:39 +0100

    maj

--- website_src/read_passage.php
+++ website_src/read_passage.php
@@ -9,10 +9,22 @@ try{
     die();
 } 
 
-$sql = 'SELECT id_categorie, id_epreuve, id_participant, MIN(meilleur_temp) FROM `passages` GROUP BY id_participant LIMIT 3';
+// recup un id epreuve pour l'insérer dans le where 
+
+// 
+$sql ='SELECT id_epreuve, id_categorie, id_participant, meilleur_temp FROM `passages` ORDER BY `passages`.`meilleur_temp` ASC LIMIT 3
+       UNION
+       SELECT epreuves.nom_epreuve FROM epreuves, passages WHERE passages.id_epreuve = epreuves.id_epreuve
+       UNION
+       SELECT categories.type FROM categories, passages  WHERE passages.id_categorie = categories.id_categorie
+       UNION
+       SELECT participants.nom_participant, participants.prenom_participant FROM participants, passages WHERE passages.id_participant = participants.id_participant';
+
+
+// 
+
 // $sql = 'SELECT MIN(meilleur_temp) FROM passages ';
 
-// `participants`,`categories`,
 // On prepare la requete
 $query = $db->prepare($sql);
 // on execute la requete
@@ -20,7 +32,7 @@ $query->execute();
 
 // on stock le result dans un tableau assoc
 $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
+var_dump($result);
 
 
 $db = null;
@@ -36,9 +48,13 @@ $db = null;
 <body>
 
 
+
 <?php foreach ($result as $results): extract($results) ?>
+<p><?= $nom_epreuve?></p>
+<p><?= $type?></p>
+<p><?= $nom_participant?></p>
+<p><?= $prenom_participant?></p>
 <p><?= $meilleur_temp?></p>
-<!-- <?= $prenom_participant?> -->
 <?php endforeach ?>  
 
 
