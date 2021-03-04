7516b17e1caabe469b979f349749935d9f753451
jctest <jcmairot@gmail.com>
2021-03-04 10:37:37 +0100

    write excel-->bdd jc

--- test_jc.php
+++ test_jc.php
@@ -1,7 +1,10 @@
 <?php
+include 'inc/bdd.php';
 include 'inc/header.php';
-include 'inc/fonctions.php';
+// include 'inc/fonctions.php';
 require 'vendor/autoload.php';
+
+
 
 echo "page de test jc";
 
@@ -10,11 +13,10 @@ $reader->setReadDataOnly(TRUE);
 $spreadsheet = $reader->load("creatXl.xlsx");
 
 $worksheet = $spreadsheet->getActiveSheet();
-// Get the highest row and column numbers referenced in the worksheet
+// recup le haut du row et  highest row and column numbers referenced in the worksheet
 $highestRow = $worksheet->getHighestRow(); // e.g. 10
 $highestColumn = $worksheet->getHighestColumn(); // e.g 'F'
 $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); // e.g. 5
-
 
 
 echo '<table>' . "\n";
@@ -34,10 +36,59 @@ for ($row = 1; $row <= $highestRow; ++$row) {
     echo '</tr>' . PHP_EOL;
 }
 echo '</table>' . PHP_EOL;
-echo '<pre>';
-print_r($values);
-echo '</pre>';
 
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//une boucle avec incrementation de values
+
+for ($i=0; $i <$highestRow ; $i++) { 
+    $bdd = getBdd();
+    $input=$i;
+    $tabs=$values[$input];
+
+    $tab=$tabs[0];
+    $id_participant=$tab;
+    $tab1=$tabs[1];
+    $nom_participant=$tab1;
+    $tab2=$tabs[2];
+    $prenom_participant=$tab2;
+    $tab3=$tabs[3];
+    $birth_participant=$tab3;
+    $tab4=$tabs[4];
+    $mail_participant=$tab4;
+
+    $request=$bdd->prepare("UPDATE participants 
+                            SET nom_participant='$nom_participant',
+                                prenom_participant='$prenom_participant',
+                                birth_participant='$birth_participant',
+                                mail_participant='$mail_participant'
+
+                            WHERE  id_participant = $id_participant");
+    $request->execute();  
+
+
+
+    // var_dump($nom_participant);
+    // var_dump($prenom_participant);
+    // var_dump($birth_participant);
+    // var_dump($mail_participant);
+
+    
+
+}
+
+
+    
+
+
+//une boucle avec incrementation tabs 
+// $tab=$tabs[0];
+// $id_participant=$tab;
+
+
+
+//-----------------------------------------------------
 
 
 ?> 
