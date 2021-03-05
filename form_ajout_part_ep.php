c85671bf72b1be186e86b09c8d1ac46caf31956b
YoannGenique <genique.yoann@outlook.com>
2021-03-05 11:59:37 +0100

    formulaire

--- form_ajout_part_ep.php
+++ form_ajout_part_ep.php
@@ -1,4 +1,5 @@
 <?php
+include 'inc/header.php';
 include 'inc/fonctions.php';
 
 $participants = getParticpant();
@@ -8,38 +9,33 @@ $categories = getCategorie();
 $epreuves = getEpreuve();
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <form action="#" method="POST">
         <div>
             <!-- Pour epreuve + cate pour aller chercher la bonne cate + ep c'est un READ SELECT -->
-            <?php foreach ($epreuves as $epreuve):?>
-                <label for="Nom epreuve"><?=$epreuve["nom_epreuve"]?></label><br>
-                <label for="Date epreuve"><?=$epreuve["date_epreuve"]?></label>
-            <?php endforeach ?>
+            <label for="Nom_epreuve">Epreuve: </label><br>
+                <select name="nom_epreuve" id="type-select"> 
+                <?php foreach ($epreuves as $epreuve):?>
+                    <option value="<?=$epreuve['id_epreuve']?>"><?=$epreuve['nom_epreuve']?></option>
+                <?php endforeach ?>
+                </select>
         </div>
         <div>
-            <?php foreach ($categories as $categorie):?>
-                <label for="Categorie"><?=$categorie["type"]?></label>
-            <?php endforeach ?>
+            <label for="Categorie">Catégorie: </label>
+                <select name="type" id="type-select"> 
+                <?php foreach ($categories as $categorie):?>
+                    <option value="<?=$categorie['id_categorie']?>"><?=$categorie['type']?></option>
+                <?php endforeach ?>
+            </select>
         </div>
         <div>
             <label for="Participant">Participant à cette épreuve:</label>
                 <?php foreach ($participants as $participant): extract($participant) ?>
                     <p><?=$nom_participant.' '.$prenom_participant?></p>
-                    <input type="checkbox" name="<?=$id_participant?>" placeholder="<?=$nom_participant?>">
+                    <input type="checkbox" name="<?=$id_participant?>">
                 <?php endforeach ?>
         </div>
         <br>
         <br>
         <input type="submit" value="Enregistrer">
     </form>
-</body>
-</html>
+<?php include 'inc/header.php'; ?>
