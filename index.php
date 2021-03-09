fc84b7c838b36566c56e7df7d9bf426a3b73f7ef
celia77940 <celiarameau@live.fr>
2021-03-09 13:32:31 +0100

    index fini

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'inc/header.php';
+?>
+
+<div class="position-absolute top-50 start-50 translate-middle border">
+    <h1 class="text-center color">API</h1>
+    <div >
+        <a href="create_Competitor.php" class="d-grid gap-2">
+           <button class="btn btn-primary" type="submit">Ajouter des Participant</button> 
+        </a>           
+    </div>
+    <div >
+        <a href="create_Proof.php" class="d-grid gap-2">
+           <button class="btn btn-primary" type="submit">Ajouter des Epreuves</button> 
+        </a>        
+    </div>
+    <div>
+        <a href="competitor_proof.php" class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit">Ajouter d'un participant à une épreuve</button>
+        </a>    
+    </div>
+    <div>
+        <a href="insert_passage.php" class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit">Importer l'exel</button>
+        </a>          
+    </div>
+    <div >
+        <a href="resultat.php" class="d-grid gap-2">
+            <button class="btn btn-primary" type="submit">Resultat</button>
+        </a>  
+    </div>
+</div>
+
+<?php
+require_once 'inc/footer.php';
+?>
